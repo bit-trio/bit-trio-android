c8c07ebb6b230dbb5544c7c43cd995457cbf5e28
nathankim0 <jinyeob07@gmail.com>
2022-06-03 14:21:36 +0900

    chore: create initial android project

---
+++ app/src/main/java/com/jinyeob/bit_trio_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jinyeob.bit_trio_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
